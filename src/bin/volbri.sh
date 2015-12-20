881f96365c9ac7c3827d93b47f7e20b0d6f69469
Konstantin Baierer <unixprog@gmail.com>
2015-12-20 02:37:23 +0100

    status, stop, start, auto-restart for daemon

--- src/bin/volbri.sh
+++ src/bin/volbri.sh
@@ -2,7 +2,37 @@
 user=$(whoami)
 SOCK=/tmp/$user/volbrid.sock
 if [[ ! -e $SOCK ]];then
-    echo "Server socket at $SOCK not found. Start server with volbrid(1)"
-    exit 12
+    echo "Server socket at $SOCK not found. Starting server with volbrid(1)."
 fi
-echo $@|socat - "UNIX-CONNECT:$SOCK"
+status() {
+    pid=$(pgrep -f volbrid)
+    if [[ -z "$pid" ]];then
+        echo "STOPPED"
+    else
+        echo "RUNNING[$pid]"
+    fi
+}
+stop() {
+    if [[ $(status) != 'STOPPED' ]];then
+        echo "Stopping volbrid"
+        pkill -ef volbrid
+    fi
+}
+start() {
+    stopped=$(stop)
+    if [[ -z "$stopped" ]];then
+        echo "Starting volbrid"
+    else
+        echo "Restarting volbrid"
+    fi
+    volbrid
+}
+if [[ -z "$1" ]];then
+    start
+elif [[ "$1" == '--quit' ]];then
+    stop
+elif [[ "$1" == '--status' ]];then
+    status
+else
+    echo $@|socat - "UNIX-CONNECT:$SOCK"
+fi
